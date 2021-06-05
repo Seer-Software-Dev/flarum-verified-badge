25ca89514c94294d7d38347bef16ee0d47176bcb
serakoi <admin@seer-software.com>
2021-06-05 14:10:49 +0200

    Add a blue version

--- extend.php
+++ extend.php
@@ -43,6 +43,20 @@ return [
                     width: auto;
                     background-repeat: no-repeat;
                 }
+                .verifiedBadgeBlue {
+                    line-height: 21px;
+                    text-align: center;
+                    font-size: 12.32px;
+                    box-shadow: none!important;
+                    margin-left: 0px !important;
+                    background-image: url(https://seer-software.com/svg/verified-blue.svg);
+                    background-color: initial;
+                    background-size: 100% auto;
+                    display: block;
+                    height: 100%;
+                    width: auto;
+                    background-repeat: no-repeat;
+                }
             </style>';
         })
 ];
